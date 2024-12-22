eb1b4e4640870ece53b101139f0f9bf95d152927
Zarutan454 <lincar640@gmail.com>
2024-12-22 16:05:22 +0100

    änderung

---
+++ client/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(__dirname));
+
+// Route for the main page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Frontend server running at http://localhost:${PORT}`);
+});
